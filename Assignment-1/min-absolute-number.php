da64ebf22b8be87e317ca13089e6821dd013081a
Md Akib Rahman <mdakibrahman6@gmail.com>
2024-06-05 06:13:33 +0600

    Revert "Assignment1 Done"
    This reverts commit 98b9e2f1b5506abd4f7190c877c4c59565c2f2eb.

--- Assignment-1/min-absolute-number.php
+++ Assignment-1/min-absolute-number.php
@@ -1,21 +1,3 @@
 <?php
-$sample_numbers1 = [10, 12, 15, 189, 22, -2, 34];
-$sample_numbers2 = [10, -12, 34, 12, -3, 123];
-
-function minAbsNumber(array $numbers)
-{
-  $minAbsNumber = $numbers[0];
-  foreach ($numbers as $key => $value) {
-    if ($minAbsNumber > $value) {
-      $minAbsNumber = $value;
-    }
-  }
-
-  if ($minAbsNumber < 0) {
-    $minAbsNumber *= -1;
-  }
-
-  return $minAbsNumber;
-}
-
-echo minAbsNumber($sample_numbers1);
+$sample_numbers = [10, 12, 15, 189, 22, 2, 34];
+$sample_numbers = [10, -12, 34, 12, -3, 123];
